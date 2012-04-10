9d4192c9f5a4bcdadd9eed402ab8913c630cb5f4
Shirin-sajjadi <sajjadi.shirin@gmail.com>
2012-04-10 15:48:53 +0430

    Adult-3

---
+++ inc/menu.php
@@ -0,0 +1,10 @@
+<div class="menu">
+		<ul>
+			<li class="Home"><a href="#"><span>Home</span></a></li>
+			<li class="Children"><a href="#"><span>Children</span></a></li>
+			<li class="Terms"><a href="#"><span>Terms</span></a></li>
+			<li class="ContactUs"><a href="#"><span>ContactUs</span></a></li>
+			<li class="Marks"><a href="#"><span>Result</span></a></li>
+			<li class="Register"><a href="#"><span>registration</span></a></li>
+		</ul>
+	</div>
